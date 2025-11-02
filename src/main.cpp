d8f8213c3e237eda0aac5d281a5ead686d23a2bd
Amir H. Pakdaman <ahpd1068@yahoo.com>
2025-11-02 16:12:02 +0100

    manual_control_with_pot

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define EN_PIN 4
+#define STEP_PIN 5
+#define DIR_PIN 6
+#define POT_PIN A0
+
+const int maxSteps = 1600;
+const int stepDelay = 800;
+const int positionTolerance = 20;
+
+int potValue = 0;
+int targetPosition = 0;
+int currentPosition = 0;
+int lastPotValue = 0;
+
+void stepMotor(bool dir) {
+  digitalWrite(DIR_PIN, dir);
+  digitalWrite(STEP_PIN, HIGH);
+  delayMicroseconds(stepDelay);
+  digitalWrite(STEP_PIN, LOW);
+  delayMicroseconds(stepDelay);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+
+  digitalWrite(DIR_PIN, LOW);
+  digitalWrite(EN_PIN, LOW);
+
+  // Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  potValue = analogRead(POT_PIN);
+  targetPosition = map(potValue, 0, 1023, 0, maxSteps);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int diff = targetPosition - currentPosition;
+
+  if (abs(diff) > positionTolerance) {
+
+    if (diff > 0) {
+      stepMotor(HIGH);
+      currentPosition++;
+    } else {
+      stepMotor(LOW);
+      currentPosition--;
+    }
+  } 
+  else {
+    digitalWrite(STEP_PIN, LOW);
+    delay(5);
+  }
+
+  lastPotValue = potValue;
 }
